8c611af32c22892cf8995bba2b499e072309fdcc
rupal_vsc <rupal_majmudar@hotmail.com>
2018-10-08 17:45:00 +0200

    rm1

--- backend/contracts/payments_rupal.sol
+++ backend/contracts/payments_rupal.sol
@@ -4,8 +4,46 @@ pragma experimental ABIEncoderV2;
 library SharedStructs {
     struct Service {
         string name;
-        uint8 price;
+        uint256 price;
     }   
+}
+
+//@courtesy https://programtheblockchain.com/posts/2017/12/15/writing-a-contract-that-handles-ether/
+contract PaymentFunctions {
+    event LogMoneyTransfer(address sender, address receiver, uint256 amount);
+        
+    function withdrawAll() public {
+        msg.sender.transfer(address(this).balance);
+    }
+
+    /*
+     * address.transfer(amount) transfers amount (in ether) 
+     * _TO_ the account represented by address.
+     */
+    function withdraw(uint256 amount) payable public {
+        msg.sender.transfer(amount);
+        emit LogMoneyTransfer(address(this), msg.sender, amount);
+    }
+    
+    function withdrawalByAddress(uint256 amount, address addr) payable public {
+        addr.transfer(amount);
+        emit LogMoneyTransfer(address(this), addr, amount);
+    }
+    
+    //@see faq: https://stackoverflow.com/questions/52003763/even-though-i-dont-have-constructor-in-my-code-i-am-getting-error-the-construct
+    function deposit(uint256 amount) payable public {
+        //require(msg.value == amount); TODO: CHK!
+        // nothing else to do!
+        emit LogMoneyTransfer(msg.sender, address(this), amount);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+    
+    function getBalance(address addr) public view returns (uint256) {
+        return addr.balance;
+    }
 }
 
 contract Provider {
@@ -16,37 +54,49 @@ contract Provider {
     address providerAddress;
         
     SharedStructs.Service[] services;
+    mapping(string => SharedStructs.Service) serviceByName;
     mapping(string => bool) servicesExisting;
         
-    constructor (address _providerAddress, string _providerName) public payable {
-        providerAddress = _providerAddress;
+    /**
+     * TODO: We use this constructor for creating a Provider for a Payment.
+     * But should not need to enter this info when deploying the Provider, right??
+     **/
+    constructor (address _providerAddress, string _providerName) public {
+        providerAddress= _providerAddress;
         providerName = _providerName;
         //services = new Service[](0); //not reqd
     }
     
-    function getName() public returns (string) {
+    function getName() public view returns (string) {
         return providerName;
     }
     
-    function getAddress() public returns (address) {
+    function getAddress() public view returns (address) {
         return providerAddress;
     }
     
-    function offerService(string serviceName, uint8 servicePrice) public {
+    function offerService(string serviceName, uint256 servicePrice) public {
         if (servicesExisting[serviceName]) return;
         
         // add service to the list of services of the given provider
         SharedStructs.Service memory srv= SharedStructs.Service(serviceName, servicePrice);
         services.push(srv);
+        serviceByName[serviceName] = srv;
         servicesExisting[serviceName] = true;
     }
     
-    function getServices() public returns (SharedStructs.Service[]) {
+    function getServices() public view returns (SharedStructs.Service[]) {
         return services;
     }
     
-    function doesServiceExist(string serviceName) public returns (bool) {
-        return servicesExisting[serviceName];
+    function doesServiceExist(string serviceName) public view returns (bool) {
+        bool exists= servicesExisting[serviceName];
+        return exists;
+    }
+    
+    function getService(string serviceName) public view returns (SharedStructs.Service) {
+        SharedStructs.Service storage srv= serviceByName[serviceName];
+        return srv;
     }
 }
 
@@ -81,14 +131,15 @@ contract Payments {
     //======================================================
     
     //From https://medium.com/loom-network/ethereum-solidity-memory-vs-storage-how-to-initialize-an-array-inside-a-struct-184baf6aa2eb
-    function addProvider(string _provName) public payable {
+    function addProvider(string _provName) public {
         if (providersExistingByName[_provName]) return;
         
         Provider newProvider = new Provider(msg.sender, _provName);
-        
+        address msg_sender= msg.sender; //for debugging. Strange 0's get prefixed!
         providersArray.push(newProvider);
         providersByName[_provName]= newProvider;
-        providersByAddress[msg.sender]= newProvider; //unique name & address for each provider. Fair enough?
+        providersByAddress[msg_sender]= newProvider; //unique name & address for each provider. Fair enough?
+        providersExistingByAddress[msg_sender] = true;
         providersExistingByName[_provName]= true;
     }
   
@@ -96,8 +147,12 @@ contract Payments {
         return providersArray;
     }
     
+    //@TODO: Review. @see https://ethereum.stackexchange.com/questions/30665/warning-uninitialized-storage-pointer
+    //@TODO: Also dunno how to get rid of "Warning: Uninitialized storage pointer"
     function getAllProviderNames() public view returns (string[]) { 
-        string[] names;
+        //uint256 len= providersArray.length + 1;
+        //string[] storage names = new string[](len);
+        string[] storage names;
         for (uint i=0; i<providersArray.length; i++) {
             names.push(providersArray[i].getName());
         }
@@ -107,7 +162,7 @@ contract Payments {
     
     //======================================================
         
-    function offerService(string providerName, string serviceName, uint8 servicePrice) public {
+    function offerService(string providerName, string serviceName, uint256 servicePrice) public {
         if (!providersExistingByName[providerName]) return;
         
         Provider provider= providersByName[providerName];
@@ -132,13 +187,21 @@ contract Payments {
     
     function makePayment(address providerAddress, string serviceName) public payable {
         if (!users[msg.sender]) return;
-        if (!providersExistingByAddress[providerAddress]) return;
+        bool exists = providersExistingByAddress[providerAddress];
+        if (!exists) return;
         
-        Provider provider= providersByAddress[providerAddress];
-        //SharedStructs.Service[] memory services= provider.getServices();
+        Provider provider = providersByAddress[providerAddress];
         if (!provider.doesServiceExist(serviceName)) return;
         
-        // make payment -> substract money from caller, send money to the provider
-        //
+        SharedStructs.Service memory service= provider.getService(serviceName);
+        uint256 serviceCost = service.price; 
+                
+        // make payment -> substract money from caller [address: msg.sender], 
+        //send money to the provider [address: providerAddress]
+        //amount: serviceCost
+        PaymentFunctions pf= new PaymentFunctions();
+        //TODO BUG - serviceCost value comes to be 0 !!
+        pf.deposit(150); //Money from the user (msg.sender) to this contract.
+        pf.withdrawalByAddress(150, providerAddress);
     }
 }
